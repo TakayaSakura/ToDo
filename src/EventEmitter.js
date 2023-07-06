e998a2457a0649ff7088daac75ec4b8c47458d42
TAkayaSakura <sakutaka0523@outlook.jp>
2023-07-06 11:10:54 +0900

    first commit

---
+++ src/EventEmitter.js
@@ -0,0 +1,34 @@
+export class EventEmitter {
+    #listners = new Map();
+    addEventListner(type, listener) {
+        if (!this.#listeners.has(type)) {
+            this.#listeners.set(type, new Set());
+        }
+        const listenerSet = this.#listeners.get(type);
+        listenerSet.add(listener);
+    };
+
+    emit(type) {
+        const listenerSet = this.#listeners.get(type);
+        if(!listenerSet) {
+            return;
+        }
+        listenerSet.forEach(listener => {
+            listener.call(this);
+        });
+    };
+
+    removeEventListener(type, listener) {
+        const listenerSet = this.#listeners.get(type);
+        if (!listenerSet) {
+            return;
+        }
+        listenerSet.forEach(ownListener => {
+            if (ownListener === listener) {
+                listenerSet.delete(listener);
+            }
+        });
+    }
+
+}
+
